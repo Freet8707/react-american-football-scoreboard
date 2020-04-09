3569bfb0ba422045d7117a04b5f055cb4c01bd8c
Jeremy Freetly <freet8707@gmail.com>
2020-04-08 22:09:53 -0500

    click functionality to home and away touchdown and field goal buttons. Project complete at MVP.

--- src/App.js
+++ src/App.js
@@ -36,8 +36,8 @@ function App() {
           <button className="homeButtons__fieldGoal" onClick = {() => {setHomeScore(fieldGoalIncrement(homeScore))}}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick = {() => {setAwayScore(touchdownIncrement(awayScore))}}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick = {() => {setAwayScore(fieldGoalIncrement(awayScore))}}>Away Field Goal</button>
         </div>
       </section>
     </div>
