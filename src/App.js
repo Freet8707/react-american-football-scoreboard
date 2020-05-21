23d73210200c20ad5976a61f2e9f7026e6fa41e4
Jeremy Freetly <freet8707@gmail.com>
2020-05-20 21:24:59 -0500

    button for updating state value of downs implemented and passed into BottomRow component using props

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
+  const [downs, setDowns] = useState(0)
 
   return (
     <div className="container">
@@ -25,13 +26,14 @@ function App() {
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
-        <BottomRow />
+        <BottomRow downs={downs}/>
       </section>
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={() => setHomeScore(homeScore + 7)}>Home Touchdown</button>
           <button className="homeButtons__fieldGoal" onClick={() => setHomeScore(homeScore + 3)}>Home Field Goal</button>
+          <button className='downsSet' onClick={() => setDowns(downs + 1)}>next down</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={() => setAwayScore(awayScore + 7)}>Away Touchdown</button>
